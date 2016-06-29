9e99bb6327979de25941c75d69d4eed3f2972f26
Valenza Florian <fvalenza@laas.fr>
2016-06-29 15:43:38 +0200

    Added functions to convert an assimp scene to a mesh. hpp-fcl is compatible with 2.0 < assimp < 3.0.0

--- ci/install_linux.sh
+++ ci/install_linux.sh
@@ -8,6 +8,12 @@ sudo apt-get -qq --yes --force-yes install cmake
 sudo apt-get -qq --yes --force-yes install libboost-all-dev
 sudo apt-get -qq --yes --force-yes install libccd-dev
 
+# Assimp
+sudo echo "deb [arch=amd64] http://robotpkg.openrobots.org/packages/debian/pub trusty robotpkg" >> /etc/apt/sources.list.d/robotpkg.list
+curl http://robotpkg.openrobots.org/packages/debian/robotpkg.key | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install robotpkg-assimp
+
 ########################
 # Optional dependencies
 ########################
@@ -22,3 +28,4 @@ cd build
 cmake ..
 make
 sudo make install
+
