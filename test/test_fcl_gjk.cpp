5bcf569d62bd2e16fc73d607975a9066a6f4b2b8
Florent Lamiraux <florent@laas.fr>
2018-11-30 15:11:53 +0100

    [test] Update test on pairs of triangles.

--- test/test_fcl_gjk.cpp
+++ test/test_fcl_gjk.cpp
@@ -43,7 +43,6 @@
 
 #include <hpp/fcl/narrowphase/narrowphase.h>
 #include <hpp/fcl/shape/geometric_shapes.h>
-#include <Mathematics/GteDistTriangle3Triangle3.h>
 
 using fcl::GJKSolver_indep;
 using fcl::TriangleP;
@@ -51,12 +50,11 @@ using fcl::Vec3f;
 using fcl::Transform3f;
 using fcl::Matrix3f;
 using fcl::FCL_REAL;
-using gte::DCPQuery;
-using gte::Triangle3;
-using gte::Vector3;
 
-typedef DCPQuery <FCL_REAL, Triangle3 <FCL_REAL>, Triangle3 <FCL_REAL> >
-Query_t;
+typedef Eigen::Matrix<FCL_REAL, Eigen::Dynamic, 1> vector_t;
+typedef Eigen::Matrix<FCL_REAL, 6, 1> vector6_t;
+typedef Eigen::Matrix<FCL_REAL, 4, 1> vector4_t;
+typedef Eigen::Matrix<FCL_REAL, Eigen::Dynamic, Eigen::Dynamic> matrix_t;
 
 struct Result
 {
@@ -69,6 +67,8 @@ typedef std::vector <Result> Results_t;
 
 BOOST_AUTO_TEST_CASE(distance_triangle_triangle_1)
 {
+  Eigen::IOFormat format (Eigen::FullPrecision, Eigen::DontAlignCols, ", ",
+                          ", ", "np.array ((", "))", "", "");
   std::size_t N = 10000;
   GJKSolver_indep solver;
   Transform3f tf1, tf2;
@@ -86,155 +86,127 @@ BOOST_AUTO_TEST_CASE(distance_triangle_triangle_1)
     TriangleP tri1 (P1, P2, P3);
     TriangleP tri2 (Q1, Q2, Q3);
 
+    bool res;
     start = clock ();
-    bool res = solver.shapeDistance (tri1, tf1, tri2, tf2, &distance, &p1, &p2);
+    res = solver.shapeDistance (tri1, tf1, tri2, tf2, &distance, &p1, &p2);
     end = clock ();
     results [i].timeGjk = end - start;
     results [i].collision = !res;
-    assert (res != (distance < 0));
-    if (distance >= 0) {
-      // Compute vectors between vertices
-      Vec3f u1 (P2 - P1);
-      Vec3f v1 (P3 - P1);
-      Vec3f w1 (u1.cross (v1));
-      Vec3f u2 (Q2 - Q1);
-      Vec3f v2 (Q3 - Q1);
-      Vec3f w2 (u2.cross (v2));
-      // Check that closest points are on triangles
-      // Compute a1 such that p1 = P1 + a11 u1 + b11 v1 + c11 u1 x v1
-      assert (w1.squaredNorm () > eps*eps);
-      M.col (0) = u1; M.col (1) = v1; M.col (2) = w1;
-      a1 = M.inverse () * (p1 - P1);
-      assert (fabs (a1 [2]) < eps);
-      assert (a1 [0] >= -eps);
-      assert (a1 [1] >= -eps);
-      assert (a1 [0] + a1 [1] <= 1+eps);
-      assert (w2.squaredNorm () > eps*eps);
-      M.col (0) = u2; M.col (1) = v2; M.col (2) = w2;
-      a2 = M.inverse () * (p2 - Q1);
-      assert (fabs (a2 [2]) < eps);
-      assert (a2 [0] >= -eps);
-      assert (a2 [1] >= -eps);
-      assert (a2 [0] + a2 [1] <= 1+eps);
-    } else {
-      ++nCol;
+    BOOST_CHECK (res == (distance >= 0));
+    if (!res) {
+      ++nCol; distance = 0;
     }
-    // Using geometric tools
-    Query_t query;
-    Vector3 <FCL_REAL> gteP1 = {P1 [0], P1 [1], P1 [2]};
-    Vector3 <FCL_REAL> gteP2 = {P2 [0], P2 [1], P2 [2]};
-    Vector3 <FCL_REAL> gteP3 = {P3 [0], P3 [1], P3 [2]};
-    Vector3 <FCL_REAL> gteQ1 = {Q1 [0], Q1 [1], Q1 [2]};
-    Vector3 <FCL_REAL> gteQ2 = {Q2 [0], Q2 [1], Q2 [2]};
-    Vector3 <FCL_REAL> gteQ3 = {Q3 [0], Q3 [1], Q3 [2]};
-    Triangle3 <FCL_REAL> gteTri1 (gteP1, gteP2, gteP3);
-    Triangle3 <FCL_REAL> gteTri2 (gteQ1, gteQ2, gteQ3);
-    start = clock ();
-    Query_t::Result gteRes = query (gteTri1, gteTri2);
-    end = clock ();
-    results [i].timeGte = end - start;
-
-    Vector3 <FCL_REAL> gte_p1 = gteRes.closestPoint [0];
-    Vector3 <FCL_REAL> gte_p2 = gteRes.closestPoint [1];
-    Vector3 <FCL_REAL> gte_p2_p1 = gte_p2 - gte_p1;
-    // assert (((distance < 0) && (gteRes.distance == 0)) ||
-    //         ((distance > 0) && (gteRes.distance > 0)));
-    assert (fabs (gteRes.sqrDistance - Dot (gte_p2_p1, gte_p2_p1)) < eps);
-    if ((distance >= 0) && (fabs (distance - gteRes.distance) >= eps)) {
-      ++nDiff;
-    } else {
-      assert ((distance < 0) ||
-              ((fabs (gte_p1 [0] - p1 [0]) < eps) &&
-               (fabs (gte_p1 [1] - p1 [1]) < eps) &&
-               (fabs (gte_p1 [2] - p1 [2]) < eps) &&
-               (fabs (gte_p2 [0] - p2 [0]) < eps) &&
-               (fabs (gte_p2 [1] - p2 [1]) < eps) &&
-               (fabs (gte_p2 [2] - p2 [2]) < eps)) ||
-              ((fabs (gte_p1 [0] - p2 [0]) < eps) &&
-               (fabs (gte_p1 [1] - p2 [1]) < eps) &&
-               (fabs (gte_p1 [2] - p2 [2]) < eps) &&
-               (fabs (gte_p2 [0] - p1 [0]) < eps) &&
-               (fabs (gte_p2 [1] - p1 [1]) < eps) &&
-               (fabs (gte_p2 [2] - p1 [2]) < eps)));
-    }
-#if 0
     // Compute vectors between vertices
-    Vector3 <FCL_REAL> gte_u1 (gteP2 - gteP1);
-    Vector3 <FCL_REAL> gte_v1 (gteP3 - gteP1);
-    Vector3 <FCL_REAL> gte_w1 (Cross (gte_u1, gte_v1));
-    Vector3 <FCL_REAL> gte_u2 (gteQ2 - gteQ1);
-    Vector3 <FCL_REAL> gte_v2 (gteQ3 - gteQ1);
-    Vector3 <FCL_REAL> gte_w2 (Cross (gte_u2, gte_v2));
-    // Check that closest points are on triangles
-    // Compute a1 such that p1 = P1 + a11 u1 + b11 v1 + c11 u1 x v1
-    assert (std::sqrt(Dot(gte_w1, gte_w1)) > eps*eps);
-    M (0,0) = gte_u1 [0]; M (1,0) = gte_u1 [1]; M (2,0) = gte_u1 [2];
-    M (0,1) = gte_v1 [0]; M (1,1) = gte_v1 [1]; M (2,1) = gte_v1 [2];
-    M (0,2) = gte_w1 [0]; M (1,2) = gte_w1 [1]; M (2,2) = gte_w1 [2];
-    p1 [0] = gte_p1 [0]; p1 [1] = gte_p1 [1]; p1 [2] = gte_p1 [2];
+    Vec3f u1 (P2 - P1);
+    Vec3f v1 (P3 - P1);
+    Vec3f w1 (u1.cross (v1));
+    Vec3f u2 (Q2 - Q1);
+    Vec3f v2 (Q3 - Q1);
+    Vec3f w2 (u2.cross (v2));
+    BOOST_CHECK (w1.squaredNorm () > eps*eps);
+    M.col (0) = u1; M.col (1) = v1; M.col (2) = w1;
+    // Compute a1 such that p1 = P1 + a11 u1 + a12 v1 + a13 u1 x v1
     a1 = M.inverse () * (p1 - P1);
-    assert (fabs (a1 [2]) < eps);
-    assert (a1 [0] >= -eps);
-    assert (a1 [1] >= -eps);
-    assert (a1 [0] + a1 [1] <= 1+eps);
-    // Check whether closest point is on triangle edges
-    if ((a1 [0] > eps) && (a1 [1] > eps) && (a1 [0] + a1 [1] < 1 - eps)) {
-      std::cerr << "a1 = " << a1.transpose () << std::endl;
-    }
-    assert (std::sqrt(Dot(gte_w2, gte_w2)) > eps*eps);
-    M (0,0) = gte_u2 [0]; M (1,0) = gte_u2 [1]; M (2,0) = gte_u2 [2];
-    M (0,1) = gte_v2 [0]; M (1,1) = gte_v2 [1]; M (2,1) = gte_v2 [2];
-    M (0,2) = gte_w2 [0]; M (1,2) = gte_w2 [1]; M (2,2) = gte_w2 [2];
-    p2 [0] = gte_p2 [0]; p2 [1] = gte_p2 [1]; p2 [2] = gte_p2 [2];
+    BOOST_CHECK (w2.squaredNorm () > eps*eps);
+    // Compute a2 such that p2 = Q1 + a21 u2 + a22 v2 + a23 u2 x v2
+    M.col (0) = u2; M.col (1) = v2; M.col (2) = w2;
     a2 = M.inverse () * (p2 - Q1);
-    assert (fabs (a2 [2]) < eps);
-    assert (a2 [0] >= -eps);
-    assert (a2 [1] >= -eps);
-    assert (a2 [0] + a2 [1] <= 1+eps);
-    // Check whether closest point is on triangle edges
-    if ((a2 [0] > eps) && (a2 [1] > eps) && (a2 [0] + a2 [1] < 1 - eps)) {
-      std::cerr << "a2 = " << a2.transpose () << std::endl;
+
+    // minimal distance and closest points can be considered as a constrained
+    // optimisation problem:
+    //
+    // min f (a1[0],a1[1], a2[0],a2[1])
+    //   g1 (a1[0],a1[1], a2[0],a2[1]) <=0
+    //   ...
+    //   g6 (a1[0],a1[1], a2[0],a2[1]) <=0
+    // with
+    //  f (a1[0],a1[1], a2[0],a2[1]) =
+    //         1                                                           2
+    //        --- dist (P1 + a1[0] u1 + a1[1] v1, Q1 + a2[0] u2 + a2[1] v2),
+    //         2
+    //  g1 (a1[0],a1[1], a2[0],a2[1]) = -a1[0]
+    //  g2 (a1[0],a1[1], a2[0],a2[1]) = -a1[1]
+    //  g3 (a1[0],a1[1], a2[0],a2[1]) =  a1[0] + a1[1] - 1
+    //  g4 (a1[0],a1[1], a2[0],a2[1]) = -a2[0]
+    //  g5 (a1[0],a1[1], a2[0],a2[1]) = -a2[1]
+    //  g6 (a1[0],a1[1], a2[0],a2[1]) =  a2[0] + a2[1] - 1
+
+    // Compute gradient of f
+    vector4_t grad_f;
+    grad_f [0] = -(p2 - p1).dot (u1);
+    grad_f [1] = -(p2 - p1).dot (v1);
+    grad_f [2] = (p2 - p1).dot (u2);
+    grad_f [3] = (p2 - p1).dot (v2);
+    vector6_t g;
+    g [0] = -a1 [0];
+    g [1] = -a1 [1];
+    g [2] = a1 [0] + a1 [1] - 1;
+    g [3] = -a2 [0];
+    g [4] = -a2 [1];
+    g [5] = a2 [0] + a2 [1] - 1;
+    matrix_t grad_g (4, 6); grad_g.setZero ();
+    grad_g (0,0) = -1.;
+    grad_g (1,1) = -1;
+    grad_g (0,2) = 1; grad_g (1,2) = 1;
+    grad_g (2,3) = -1;
+    grad_g (3,4) = -1;
+    grad_g (2,5) = 1; grad_g (3,5) = 1;
+    // Check that closest points are on triangles planes
+    // Projection of [P1p1] on line normal to triangle 1 plane is equal to 0
+    BOOST_CHECK (fabs (a1 [2]) < eps);
+    // Projection of [Q1p2] on line normal to triangle 2 plane is equal to 0
+    BOOST_CHECK (fabs (a2 [2]) < eps);
+
+    /* Check Karush–Kuhn–Tucker conditions
+                    6
+                   __
+                   \
+         -grad f = /_  c  grad g
+                   i=1  i       i
+
+         where c  >= 0, and
+                i
+         c  g  = 0 for i between 1 and 6
+          i  i
+    */
+
+    matrix_t Mkkt (4, 6); matrix_t::Index col = 0;
+    // Check that constraints are satisfied
+    for (vector6_t::Index j=0; j<6; ++j) {
+      BOOST_CHECK (g [j] <= eps);
+      // if constraint is saturated, add gradient in matrix
+      if (fabs (g [j]) <= eps) {
+        Mkkt.col (col) = grad_g.col (j); ++col;
+      }
     }
-#endif
+    if (col > 0) {
+      Mkkt.conservativeResize (4, col);
+      // Compute KKT coefficients ci by inverting
+      // Mkkt.c = -grad_f
+      Eigen::JacobiSVD <matrix_t> svd
+        (Mkkt, Eigen::ComputeThinU | Eigen::ComputeThinV);
+      vector_t c (svd.solve (-grad_f));
+      for (vector_t::Index j=0; j < c.size (); ++j) {
+        BOOST_CHECK (c [j] >= -eps);
+      }
+    }
   }
   std::cerr << "nCol = " << nCol << std::endl;
   std::cerr << "nDiff = " << nDiff << std::endl;
   // statistics
-  clock_t totalTimeGjk = 0, totalTimeGte = 0;
-  clock_t totalTimeGjkColl = 0, totalTimeGteColl = 0;
-  clock_t totalTimeGjkNoColl = 0, totalTimeGteNoColl = 0;
-  std::size_t nGjkBetterThanGteColl = 0;
-  std::size_t nGjkBetterThanGteNoColl = 0;
+  clock_t totalTimeGjkColl = 0;
+  clock_t totalTimeGjkNoColl = 0;
   for (std::size_t i=0; i<N; ++i) {
     if (results [i].collision) {
       totalTimeGjkColl += results [i].timeGjk;
-      totalTimeGteColl += results [i].timeGte;
-      if ((results [i].timeGjk < results [i].timeGte)) {
-        ++ nGjkBetterThanGteColl;
-      }
     } else {
       totalTimeGjkNoColl += results [i].timeGjk;
-      totalTimeGteNoColl += results [i].timeGte;
-      if ((results [i].timeGjk < results [i].timeGte)) {
-        ++ nGjkBetterThanGteNoColl;
-      }
     }
   }
   std::cerr << "Total time gjk: " << totalTimeGjkNoColl + totalTimeGjkColl
             << std::endl;
-  std::cerr << "Total time gte: " << totalTimeGteNoColl + totalTimeGteColl
-            << std::endl;
-  std::cerr << "Number times GJK better than Geometric tools: "
-            << nGjkBetterThanGteNoColl + nGjkBetterThanGteColl << std::endl;
   std::cerr << "-- Collisions -------------------------" << std::endl;
   std::cerr << "Total time gjk: " << totalTimeGjkColl << std::endl;
-  std::cerr << "Total time gte: " << totalTimeGteColl << std::endl;
-  std::cerr << "Number times GJK better than Geometric tools: "
-            << nGjkBetterThanGteColl << std::endl;
   std::cerr << "-- No collisions -------------------------" << std::endl;
   std::cerr << "Total time gjk: " << totalTimeGjkNoColl << std::endl;
-  std::cerr << "Total time gte: " << totalTimeGteNoColl << std::endl;
-  std::cerr << "Number times GJK better than Geometric tools: "
-            << nGjkBetterThanGteNoColl << std::endl;
-
 }
