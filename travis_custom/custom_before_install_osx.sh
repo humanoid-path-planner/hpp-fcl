fa1d68641cb6e1af6cb1c214675dc5f60140895a
Gabriele Buondonno <gbuondon@laas.fr>
2019-11-01 18:19:01 +0100

    [travis] Handle Mac OS X

---
+++ travis_custom/custom_before_install_osx.sh
@@ -0,0 +1,9 @@
+# Add gepetto tap
+brew tap gepetto/homebrew-gepetto
+
+# install eigenpy
+brew install eigenpy
+
+# set environment variables
+export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/openrobots/lib/pkgconfig"
+export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/openrobots/lib"
