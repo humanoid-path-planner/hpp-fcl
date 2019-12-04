8e8d5a859b009d7753d12a1300febd71d7c75511
Justin Carpentier <justin.carpentier@inria.fr>
2019-12-04 22:25:01 +0100

    travis/osx: force overwritting of links

--- travis_custom/custom_before_install_osx.sh
+++ travis_custom/custom_before_install_osx.sh
@@ -4,8 +4,11 @@ brew update
 brew tap gepetto/homebrew-gepetto
 
 # install eigenpy
-brew install boost assimp eigen octomap eigenpy
 
-# set environment variables
-export PKG_CONFIG_PATH="${PKG_CONFIG_PATH}:/opt/openrobots/lib/pkgconfig"
-export LD_LIBRARY_PATH="${LD_LIBRARY_PATH}:/opt/openrobots/lib"
+brew install python@2
+brew install boost assimp eigen octomap 
+
+brew uninstall numpy
+pip uninstall numpy -y
+brew install eigenpy
+brew link --overwrite numpy@1.16
