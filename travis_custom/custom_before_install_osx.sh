47a1e7ebd69bdf4f58ccab14a486ab3d756cb411
Justin Carpentier <justin.carpentier@inria.fr>
2020-11-20 16:21:43 +0100

    ci: fix travis for OSX

--- travis_custom/custom_before_install_osx.sh
+++ travis_custom/custom_before_install_osx.sh
@@ -2,12 +2,4 @@ brew update
 
 # Add gepetto tap
 brew tap gepetto/homebrew-gepetto
-
-# install eigenpy
-
-brew install boost assimp eigen octomap 
-
-brew uninstall numpy
-pip uninstall numpy -y
-brew install eigenpy@2
-brew link --overwrite numpy@1.16
+brew install hpp-fcl --only-dependencies
