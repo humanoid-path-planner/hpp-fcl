31a4ecb161a2fae2ce70f57477ecdf913bb4aa27
Justin Carpentier <justin.carpentier@inria.fr>
2021-03-20 19:01:23 +0100

    serialization: fix serialization of BVHModelBase

--- include/hpp/fcl/serialization/BVH_model.h
+++ include/hpp/fcl/serialization/BVH_model.h
@@ -6,7 +6,9 @@
 #define HPP_FCL_SERIALIZATION_BVH_MODEL_H
 
 #include "hpp/fcl/BVH/BVH_model.h"
+
 #include "hpp/fcl/serialization/fwd.h"
+#include "hpp/fcl/serialization/collision_object.h"
 
 namespace boost
 {
@@ -28,11 +30,21 @@ namespace boost
               const hpp::fcl::BVHModelBase & bvh_model,
               const unsigned int /*version*/)
     {
+      using namespace hpp::fcl;
+      if(bvh_model.build_state != BVH_BUILD_STATE_PROCESSED && bvh_model.build_state != BVH_BUILD_STATE_UPDATED)
+      {
+        throw std::invalid_argument("The BVH model is not in a BVH_BUILD_STATE_PROCESSED or BVH_BUILD_STATE_UPDATED state. Could not be serialized.");
+      }
+      
+      ar & make_nvp("base",boost::serialization::base_object<hpp::fcl::CollisionGeometry>(bvh_model));
       ar & make_nvp("num_tris",bvh_model.num_tris);
       ar & make_nvp("num_vertices",bvh_model.num_vertices);
       ar & make_nvp("tri_indices",make_array(bvh_model.tri_indices,bvh_model.num_tris));
       ar & make_nvp("vertices",make_array(bvh_model.vertices,bvh_model.num_vertices));
       ar & make_nvp("build_state",bvh_model.build_state);
+      
+      ar & make_nvp("num_tris_allocated",bvh_model.num_tris);
+      ar & make_nvp("num_vertices_allocated",bvh_model.num_vertices);
       
       if(bvh_model.prev_vertices)
       {
@@ -45,6 +57,17 @@ namespace boost
         const bool has_prev_vertices = false;
         ar & make_nvp("has_prev_vertices",has_prev_vertices);
       }
+      
+      if(bvh_model.convex)
+      {
+        const bool has_convex = true;
+        ar << make_nvp("has_convex",has_convex);
+      }
+      else
+      {
+        const bool has_convex = false;
+        ar << make_nvp("has_convex",has_convex);
+      }
     }
     
     template <class Archive>
@@ -53,6 +76,8 @@ namespace boost
               const unsigned int /*version*/)
     {
       using namespace hpp::fcl;
+      
+      ar >> make_nvp("base",boost::serialization::base_object<hpp::fcl::CollisionGeometry>(bvh_model));
       ar >> make_nvp("num_tris",bvh_model.num_tris);
       if(bvh_model.num_tris > 0)
         bvh_model.tri_indices = new Triangle[bvh_model.num_tris];
@@ -63,6 +88,9 @@ namespace boost
       ar >> make_nvp("vertices",make_array(bvh_model.vertices,bvh_model.num_vertices));
       ar >> make_nvp("build_state",bvh_model.build_state);
       
+      ar & make_nvp("num_tris_allocated",bvh_model.num_tris);
+      ar & make_nvp("num_vertices_allocated",bvh_model.num_vertices);
+      
       bool has_prev_vertices;
       ar >> make_nvp("has_prev_vertices",has_prev_vertices);
       if(has_prev_vertices)
@@ -70,6 +98,9 @@ namespace boost
         bvh_model.prev_vertices = new Vec3f[bvh_model.num_vertices];
         ar >> make_nvp("prev_vertices",make_array(bvh_model.prev_vertices,bvh_model.num_vertices));
       }
+      
+      bool has_convex = true;
+      ar >> make_nvp("has_convex",has_convex);
     }
     
     HPP_FCL_SERIALIZATION_SPLIT(hpp::fcl::BVHModelBase)
