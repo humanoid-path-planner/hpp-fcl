ac5f91ddf82a0d07f1452b0f90aa120a0b1f81c9
Justin Carpentier <justin.carpentier@inria.fr>
2021-11-03 00:21:51 +0100

    core: fix function signatures
    Less compilation warnings

--- include/hpp/fcl/BVH/BVH_utility.h
+++ include/hpp/fcl/BVH/BVH_utility.h
@@ -77,19 +77,19 @@ HPP_FCL_DLLAPI
 BVHModel<KDOP<24> >* BVHExtract(const BVHModel<KDOP<24> >& model, const Transform3f& pose, const AABB& aabb);
 
 /// @brief Compute the covariance matrix for a set or subset of points. if ts = null, then indices refer to points directly; otherwise refer to triangles
-HPP_FCL_DLLAPI void getCovariance(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, int n, Matrix3f& M);
+HPP_FCL_DLLAPI void getCovariance(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, unsigned int n, Matrix3f& M);
 
 /// @brief Compute the RSS bounding volume parameters: radius, rectangle size and the origin, given the BV axises.
-HPP_FCL_DLLAPI void getRadiusAndOriginAndRectangleSize(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, int n, const Matrix3f& axes, Vec3f& origin, FCL_REAL l[2], FCL_REAL& r);
+HPP_FCL_DLLAPI void getRadiusAndOriginAndRectangleSize(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, unsigned int n, const Matrix3f& axes, Vec3f& origin, FCL_REAL l[2], FCL_REAL& r);
 
 /// @brief Compute the bounding volume extent and center for a set or subset of points, given the BV axises.
-HPP_FCL_DLLAPI void getExtentAndCenter(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, int n, Matrix3f& axes, Vec3f& center, Vec3f& extent);
+HPP_FCL_DLLAPI void getExtentAndCenter(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, unsigned int n, Matrix3f& axes, Vec3f& center, Vec3f& extent);
 
 /// @brief Compute the center and radius for a triangle's circumcircle
 HPP_FCL_DLLAPI void circumCircleComputation(const Vec3f& a, const Vec3f& b, const Vec3f& c, Vec3f& center, FCL_REAL& radius);
 
 /// @brief Compute the maximum distance from a given center point to a point cloud
-HPP_FCL_DLLAPI FCL_REAL maximumDistance(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, int n, const Vec3f& query);
+HPP_FCL_DLLAPI FCL_REAL maximumDistance(Vec3f* ps, Vec3f* ps2, Triangle* ts, unsigned int* indices, unsigned int n, const Vec3f& query);
 
 }
 
