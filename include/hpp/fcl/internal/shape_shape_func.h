a1ed8d1018b690e1afed159c1071db6a50ece6a8
Louis Montaut <louismontaut@gmail.com>
2022-07-22 10:35:02 +0200

    core: add nearest_points in Contact & expose to python

--- include/hpp/fcl/internal/shape_shape_func.h
+++ include/hpp/fcl/internal/shape_shape_func.h
@@ -84,8 +84,8 @@ struct ShapeShapeCollider {
         const Vec3f& p1 = distanceResult.nearest_points[0];
         const Vec3f& p2 = distanceResult.nearest_points[1];
 
-        Contact contact(o1, o2, distanceResult.b1, distanceResult.b2,
-                        (p1 + p2) / 2, distanceResult.normal, distance);
+        Contact contact(o1, o2, distanceResult.b1, distanceResult.b2, p1, p2,
+                        distanceResult.normal, distance);
 
         result.addContact(contact);
       }
