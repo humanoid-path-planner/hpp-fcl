6467749fdc469bf6f296fa6209f0aeea71c0d0a2
Justin Carpentier <justin.carpentier@inria.fr>
2023-04-07 18:58:19 +0200

    distance: add distance_function_not_implemented

--- src/distance_func_matrix.cpp
+++ src/distance_func_matrix.cpp
@@ -66,6 +66,22 @@ FCL_REAL Distance(const CollisionGeometry* o1, const Transform3f& tf1,
 
 #endif
 
+HPP_FCL_LOCAL FCL_REAL distance_function_not_implemented(
+    const CollisionGeometry* o1, const Transform3f& /*tf1*/,
+    const CollisionGeometry* o2, const Transform3f& /*tf2*/,
+    const GJKSolver* /*nsolver*/, const DistanceRequest& /*request*/,
+    DistanceResult& /*result*/) {
+  NODE_TYPE node_type1 = o1->getNodeType();
+  NODE_TYPE node_type2 = o2->getNodeType();
+
+  HPP_FCL_THROW_PRETTY("Distance function between node type "
+                           << std::string(get_node_type_name(node_type1))
+                           << " and node type "
+                           << std::string(get_node_type_name(node_type2))
+                           << " is not yet supported.",
+                       std::invalid_argument);
+}
+
 template <typename T_SH1, typename T_SH2>
 FCL_REAL ShapeShapeDistance(const CollisionGeometry* o1, const Transform3f& tf1,
                             const CollisionGeometry* o2, const Transform3f& tf2,
