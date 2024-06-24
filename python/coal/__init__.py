872b2f84c0ad12ef707b4bb6bf9417c68b2bada1
Louis Montaut <louismontaut@gmail.com>
2024-06-24 15:21:49 +0200

    python: fix windows build

--- python/coal/__init__.py
+++ python/coal/__init__.py
@@ -32,5 +32,30 @@
 #  POSSIBILITY OF SUCH DAMAGE.
 
 # ruff: noqa: F401, F403
-from .coal_pywrap import *
-from .coal_pywrap import __raw_version__, __version__
+
+# On Windows, if coal.dll is not in the same directory than
+# the .pyd, it will not be loaded.
+# We first try to load coal, then, if it fail and we are on Windows:
+#  1. We add all paths inside COAL_WINDOWS_DLL_PATH to DllDirectory
+#  2. If COAL_WINDOWS_DLL_PATH we add the relative path from the
+#     package directory to the bin directory to DllDirectory
+# This solution is inspired from:
+#  - https://github.com/PixarAnimationStudios/OpenUSD/pull/1511/files
+#  - https://stackoverflow.com/questions/65334494/python-c-extension-packaging-dll-along-with-pyd
+# More resources on https://github.com/diffpy/pyobjcryst/issues/33
+try:
+    from .coal_pywrap import *  # noqa
+    from .coal_pywrap import __raw_version__, __version__
+except ImportError:
+    import platform
+
+    if platform.system() == "Windows":
+        from .windows_dll_manager import build_directory_manager, get_dll_paths
+
+        with build_directory_manager() as dll_dir_manager:
+            for p in get_dll_paths():
+                dll_dir_manager.add_dll_directory(p)
+            from .coal_pywrap import *  # noqa
+            from .coal_pywrap import __raw_version__, __version__  # noqa
+    else:
+        raise
