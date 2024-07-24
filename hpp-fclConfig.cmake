3e7eaacddf019717739e734d37e64ccfa3980bcd
Guilhem Saurel <guilhem.saurel@laas.fr>
2024-07-24 22:03:17 +0200

    CMake hpp-fcl compat: fix for CMake < 3.18

--- hpp-fclConfig.cmake
+++ hpp-fclConfig.cmake
@@ -3,4 +3,15 @@
 message(WARNING "Please update your CMake from 'hpp-fcl' to 'coal'")
 
 find_package(coal REQUIRED)
-add_library(hpp-fcl::hpp-fcl ALIAS coal::coal)
+
+if(CMAKE_VERSION VERSION_LESS "3.18.0")
+  if(NOT TARGET hpp-fcl::hpp-fcl)
+    add_library(hpp-fcl::hpp-fcl SHARED IMPORTED)
+    target_link_libraries(hpp-fcl::hpp-fcl INTERFACE coal::coal)
+    get_property(_cfg TARGET coal::coal PROPERTY IMPORTED_CONFIGURATIONS)
+    get_property(_loc TARGET coal::coal PROPERTY "IMPORTED_LOCATION_${_cfg}")
+    set_property(TARGET hpp-fcl::hpp-fcl PROPERTY IMPORTED_LOCATION "${_loc}")
+  endif()
+else()
+  add_library(hpp-fcl::hpp-fcl ALIAS coal::coal)
+endif()
